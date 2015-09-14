3070b42a78f00d26b9dc3fb00903734659462fe2
MrNoahz <noahzgames@gmail.com>
2015-09-14 18:15:26 +0200

    Did stuff

--- create.php
+++ create.php
@@ -7,7 +7,7 @@
 	<body>
 		<?php 
 			$posts = json_decode(file_get_contents('./data.json'), true);
-			$array = array('title' => $_POST['title'], 'content' => $_POST['content']);
+			$array = array('title' => $_POST['title'], 'content' => $_POST['content']), 'date' => date('jS \of F - Y'));
 			$posts['posts'][count($posts['posts'])] = $array;
 			$json_output = json_encode($posts);
 			$json_file = fopen('data.json', 'w');
--- index.php
+++ index.php
@@ -1,31 +1,44 @@
 <html>
 	<head>
-		<title>shitty cms title</title>
-		<link rel="stylesheet" href="shitty_cms_style.css">
+		<title>Leo's Blog</title>
+		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/css/materialize.min.css">
+  		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/js/materialize.min.js"></script>
 	</head>
 	
 	<body>
-		<h1 class="heading">shitty cms heading</h1>
-		<?php 
-			$posts = json_decode(file_get_contents('./data.json'), true);
+		<h1 class="heading">Leo's Blog</h1>
+		<div class="contentstuff">
+			<?php 
+			$posts = json_decode(file_get_contents('./data.json'), true);	
+				//display post at index
+				function displayPost ($index) {		
+					global $posts;
+					print("
+						<div class='card'>
+							<div class='cardcontent'>
+								<h4>" . $posts['posts'][$index]['title'] . "</h4>
+								<p>" . $posts['posts'][$index]['content'] . "</p>
+							</div>
+							
+							<div class='cardfooter'>
+								<div class='minilogo'></div>
+								<div class='cardinfo'>Leo<br>" . $posts['posts'][$index]['date'] . "</div>
+							</div>
+						</div>
+					");
+				}
 			
-			//display post at index
-			function displayPost ($index) {		
-				global $posts;
-				print("
-					<div class='card'>
-						<h1>" . $posts['posts'][$index]['title'] . "</h1>
-						<p>" . $posts['posts'][$index]['content'] . "</p>
-					</div>
-				");
-			}
-			
-			for ($i = count($posts['posts']) - 1; $i >= 0; $i--) {
-				displayPost($i);
-			}
-		?>
-		<footer>
-			<p class="left text">Made by <a class="no-link" href="http://www.twitter.com/qwertxzy">qwertxzy</a></p>
-		</footer>
+				for ($i = count($posts['posts']) - 1; $i >= 0; $i--) {
+					displayPost($i);
+				}
+			?>
+		</div>
+		
+		<div class="footer">
+			<div class="container">
+				<span class="grey-text">Made by Qwertxzy. CSS made by Noahz.</span>
+			</div>
+		</div>
 	</body>
 </html>
